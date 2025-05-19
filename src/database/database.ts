aa324d81472759d95bb0bba102e6e8600a6cdedd
stefan <stefan.holmberg@systementor.se>
2025-05-19 17:01:34 +0200

    a

--- src/database/database.ts
+++ src/database/database.ts
@@ -5,6 +5,41 @@ export interface ProductId extends RowDataPacket {
     id: number;
 }
 
+export interface CategoryId extends RowDataPacket {
+    id: number;
+}
+
+
+export async function getCategoryIdFromDatabase(pimid:string) : Promise<CategoryId|undefined>{
+    const conn = await connection;
+    
+    const [rows] = await conn.query<CategoryId[]>("SELECT id from category where pimid=?", [pimid])
+    if (rows.length == 0){
+        return undefined
+    }
+    return rows[0]
+}
+
+
+export async function insertCategory(title:string, description:string, pimid:string){
+    // vi hjar ju skapat en koklumn i Products spom heter description2
+    // också en som heter color2
+    const conn = await connection;
+    await conn.execute("INSERT INTO category(description,name,pimid) VALUES(?,?,?)",[description,title,pimid])
+
+}
+
+
+export async function updateCategory(id:number, title:string, description:string){
+    // vi hjar ju skapat en koklumn i Products spom heter description2
+    // också en som heter color2
+    const conn = await connection;
+    await conn.execute("UPDATE category SET description=?, name=? WHERE id=?",[description,title,id])
+
+}
+
+
+
 export async function getProductId(name:string) : Promise<ProductId|undefined>{
    // SELECT id * FROM Products WHERE title=productName
     const conn = await connection;
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,25 @@
 import path from "path";
 import * as fs from "fs";
 import {parse} from "csv-parse"
-import { getProductId, updateProductDescription } from "./database/database";
+import { getCategoryIdFromDatabase, getProductId, insertCategory, updateCategory, updateProductDescription } from "./database/database";
 import { exit } from "process";
 
+// SKriva ett program som kan köra "lite då och då" - natten k 03:00 ALT så körs dem hela tiden
+// hämta alla categorier från Cosmic -> och spara i MySQL categories
+
+// hämta alla products från Cosmic -> och spara i MySQL products
+
+// let response = await fetch("https://api.cosmicjs.com/v3/buckets/myshop-production/objects?pretty=true&query=%7B%22type%22:%22products%22%7D&limit=10&skip=0&read_key=HBkAGdMLUy8fucWd6E6aZariQXM3UHTXUHiceVunJg04h0D5Ro&depth=1&props=slug,title,metadata,type,")
+// let data = await response.json()
+
+// console.log(data.objects[0])
 
 import { createBucketClient } from '@cosmicjs/sdk';
 
 
 let COSMIC_BUCKET_SLUG="my-project-production-74aeaa30-2cfc-11f0-9ca8-cf720a88b3b0"
 let COSMIC_READ_KEY="3FJzEHvtNMEworphl5XnNGzdzw6evjPTsiyp5SPhG3ctKNaPF8"
-let COSMIC_WRITE_KEY="n6Z1cZnunQdzZGWPidXTc3n7iRTAqWTTpVlTqKVxnJ0qOMZbTM"
+//let COSMIC_WRITE_KEY="n6Z1cZnunQdzZGWPidXTc3n7iRTAqWTTpVlTqKVxnJ0qOMZbTM"
 
 const cosmic = createBucketClient({
   bucketSlug: COSMIC_BUCKET_SLUG,
@@ -20,19 +29,37 @@ const cosmic = createBucketClient({
 
 
 
-let res2 = await cosmic.objects
-  .find({
-    type: 'categories',
-  })
-  .sort('created_at asc')
-  .limit(10);
- 
+
+let response = await cosmic.objects.find({
+                                        type: 'categories',
+                                    })
+                                      .sort('created_at asc')
+                                      .limit(100);
+
+for(const pimCatgory of response.objects){
+  console.log(pimCatgory);
+  // inte MYSQL UPSERT  utan
+  const categoryIdInDatabase = await getCategoryIdFromDatabase(pimCatgory.id)
+  console.log(categoryIdInDatabase)
+  if(categoryIdInDatabase === undefined)  {
+    // INSERT
+    await  insertCategory(pimCatgory.title,pimCatgory.metadata.description, pimCatgory.id)
+    
+  }else{
+    // UPDATE category .... WHERE id=categoryIdInDatabase
+    await  updateCategory(  categoryIdInDatabase.id, pimCatgory.title,pimCatgory.metadata.description)
+  }
 
 
 
-  console.log(res2.objects);
-  console.log(res2.total);
+  // kolla oim den finns i databasen - om så gör update
+  // SELECT * from Categry where pimid='681e484108956c95e3c7192b'  // annars gör en insert
 
+}
+
+//   console.log(res2.total);
+
+exit();
 
 
 
